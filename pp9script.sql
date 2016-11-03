ba53346dd5651d38b8e02d571952071c56bb8990
Schleder <zschled@ilstu.edu>
2016-11-03 14:54:03 -0500

    added the start to pp9

---
+++ pp9script.sql
@@ -0,0 +1,6 @@
+/*Select all data contained in the BOOKs table */
+SELECT * FROM TABLE BOOKS;
+
+/*Create a list containing publisher name, contact person, publisher phone */
+
+SELECT pubName, contact AS "Contact Person";
