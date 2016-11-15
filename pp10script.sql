c9f5296f5b3d77062104d04710112a694c1587ce
Schleder <zschled@ilstu.edu>
2016-11-15 15:14:39 -0600

    finished number one on pp10

--- pp10script.sql
+++ pp10script.sql
@@ -1,1 +1,12 @@
 /* PP10 */
+
+/* title of each book and the name and phone number of the contact at the publishers office*/
+/* Traditional Approach */
+SELECT p.name, p.phone, b.title
+	FROM PUBLISHER p JOIN BOOKS b USING(PubId);
+
+/* using WHERE clause */
+SELECT p.name, p.phone, b.title
+	FROM PUBLISHER p, BOOKS b
+	WHERE p.PubID = b.PubId;
+		
