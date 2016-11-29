8f777355a0526a0915ead9a9457ecb8c0fa48b8d
Zachary Schleder <Zach@Zacharys-MacBook-Pro-3.local>
2016-11-29 14:02:24 -0600

    added the base for prjoect phase 3

---
+++ phase3.sql
@@ -0,0 +1,13 @@
+/*List the customer name and phone number for every customer that owns a BMW, Honda or Toyota.*/
+
+
+/*List the part number and part description for every part that has less than 10 in stock.*/
+
+
+/*List the customer name and vehicle serial number for those customers who have had more than one repair order.
+
+
+/*List the repair order number, customer first name, last name, vehicle make, and date of all repair orders within the month of November 2005.  Sort the results by date, and then by customer last name.*/
+
+
+/*List the value of the parts inventory currently in stock.  For each part, list the part number, unit cost, quantity in stock and the total value of the quantity in stock (quantity in stock X unit cost).*/
